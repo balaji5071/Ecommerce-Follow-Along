63932314e838926ea058d8466cbb2a429531874c
Balaji <balajich0582gmail.com>
2025-01-31 09:53:18 +0530

    milestone-2

---
+++ front-end/Components/Login/Login.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const Loing = () => {
+  return (
+    <div>Loing</div>
+  )
+}
+
+export default Loing
---
+++ front-end/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,11 @@
+import Login from "../../Components/Login/Login";
+
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>LoginPage</div>
+  )
+}
+
+export default LoginPage
