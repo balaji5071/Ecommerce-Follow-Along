947cd551d5aa95f89354ce264c16c2ec331b4455
Balaji <balajich0582gmail.com>
2025-02-06 21:53:58 +0530

    redone

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,21 +1,30 @@
-const app=require('./app')
-const connectDb=require("./db/db")
-
-process.on('uncaughtException',(error)=>{
-    console.log('server down $ {error.message}')
-    process.exit(0)
-})
-
-
-
-
-if(process.env.NODE_ENV !== 'PRODUCTION')
-    require('dotenv').config({path:'config/.env'})
-
-
+const app =require('./app')
+const connectDb=require('./db/db')
 connectDb()
 
 
-app.listen(process.env.PORT,()=>{
-console.log('serve successfully  listen a port http://localhost:${process.env.port}')
+ 
+
+process.on("uncaughtException",(err)=>{
+    console.log(`error: ${err.message}`)
+    console.log(`submitting down the server for handling the uncaught exception`)
+
+})
+
+if(process.env.NODE_ENV!=='PRODUCTION'){
+    require('dotenv').config({path:'backend/config/.env'})
+}
+
+
+const server =app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on http://localhost:${process.env.PORT}`)
+})
+
+//unhandled promise rejection
+process.on('unhandledRejection',(err)=>{
+    console.log(`server is shuttimg down for ${err.message}`)
+
+    server.close(()=>{
+        process.exit(1);
+    })
 })
