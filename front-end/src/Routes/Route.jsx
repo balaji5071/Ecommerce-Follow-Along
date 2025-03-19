dcd0d450598f4e01b414561ce817f57644a7be93
Balaji <balajich0582gmail.com>
2025-03-19 10:18:15 +0530

    work done

--- front-end/src/Routes/Route.jsx
+++ front-end/src/Routes/Route.jsx
@@ -2,4 +2,8 @@ import LoginPage from "../Pages/LoginPage/Login";
 import SignupPage from "../Pages/SignupPage/Signup";
 import Home from "../Pages/Home";
 import product from "../Pages/product"
-export {LoginPage,SignupPage,Home,product}
+import ProductDetail from "../Pages/myProduct";
+import Cart from "../Pages/cart";
+import CreateAddress from '../pages/createAddress';
+import ActivationPage from '../pages/Activation';
+export {LoginPage,SignupPage,Home,product,ProductDetail,Cart,ActivationPage,CreateAddress}
